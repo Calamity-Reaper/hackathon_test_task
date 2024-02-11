92fc678c93d0797f6937315f50c9281295f0a75f
CaveC0der <wasylkoretskyi@gmail.com>
2024-02-11 23:52:27 +0200

    prisma: seed cleanup

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -1,25 +1,4 @@
 import { PrismaClient } from '@prisma/client';
-import { hash } from 'bcryptjs';
-
-export const randomInt = (min: number, max: number) => {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-};
-
-function shuffle(array: any[]) {
-  let currentIndex = array.length,
-    randomIndex;
-
-  while (currentIndex > 0) {
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex--;
-
-    [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
-  }
-
-  return array;
-}
 
 const prisma = new PrismaClient();
 
@@ -49,31 +28,6 @@ const main = async () => {
 
   for (const c of categories) {
     await prisma.category.create({ data: { name: c } });
-  }
-
-  const user = await prisma.user.create({
-    data: { username: 'First', email: 'first@mail.com', password: await hash('password', 8) },
-  });
-
-  for (let i = 0; i < 30; i++) {
-    await prisma.lot.create({
-      data: {
-        name: `Lot #${i}`,
-        startBid: randomInt(100, 500),
-        minPitch: randomInt(1, 50),
-        closesAt: new Date('2024-02-17'),
-        sellerId: user.id,
-        categories: {
-          create: [
-            { category: { connect: { name: categories[0] } } },
-            { category: { connect: { name: categories[1] } } },
-            { category: { connect: { name: categories[2] } } },
-            { category: { connect: { name: categories[3] } } },
-          ],
-        },
-      },
-    });
-    shuffle(categories);
   }
 };
 
