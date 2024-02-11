ef8c401d5351313ee223389ee4c1e8dabcec0f40
CaveC0der <wasylkoretskyi@gmail.com>
2024-02-11 18:23:41 +0200

    patch: bids

--- server/src/bids/bids.module.ts
+++ server/src/bids/bids.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { BidsService } from './bids.service';
 import { BidsController } from './bids.controller';
+import { PrismaModule } from '../prisma/prisma.module';
+import { LotsModule } from '../lots/lots.module';
 
 @Module({
+  imports: [PrismaModule, LotsModule],
   providers: [BidsService],
   controllers: [BidsController],
 })
